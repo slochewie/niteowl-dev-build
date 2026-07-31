60e1594ce7d6a5cde72d50e4c7e3a42036e087bf
Aaron Wilson <65009367+slochewie@users.noreply.github.com>
2026-07-31 00:46:26 -0700

    Delete admin-zip/app directory

--- admin-zip/app/css-noop-loader.mjs
+++
@@ -1,6 +0,0 @@
-export async function load(url, context, nextLoad) {
-  if (/\.css(\?.*)?$/.test(url) && url.includes('/node_modules/')) {
-    return { format: 'module', shortCircuit: true, source: '' }
-  }
-  return nextLoad(url, context)
-}
